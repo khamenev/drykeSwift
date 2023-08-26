0484fff5414a324b04c1da03ad1a951d61908ef1
khamenev <32979285+khamenev@users.noreply.github.com>
2023-08-26 20:12:06 +0300

    Initial Commit

---
+++ dryke/drykeApp.swift
@@ -0,0 +1,17 @@
+//
+//  drykeApp.swift
+//  dryke
+//
+//  Created by Sergey Khamenev on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct drykeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
